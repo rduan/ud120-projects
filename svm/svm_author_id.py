5dc9a6e8982bb3f26a5d0ce66f2a9098757a169b
YQ Duan <dxloader@gmail.com>
2018-07-21 22:18:17 +1000

    counter

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -18,6 +18,8 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
 
 
 
@@ -25,14 +27,39 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 from sklearn import svm
-
-clf=svm.SVC(kernel='linear')
-clf.fit(features_train,labels_train)
-pre = clf.predict(features_test)
-
 from sklearn.metrics import accuracy_score
-print(accuracy_score(labels_test, pre))
+from collections import Counter
 
+# clf=svm.SVC(kernel='linear')
+# clf=svm.SVC(kernel='rbf', C=10)
+# clf.fit(features_train,labels_train)
+# pre10 = clf.predict(features_test)
+# print(accuracy_score(labels_test, pre10))
+
+# clf=svm.SVC(kernel='rbf', C=100)
+# clf.fit(features_train,labels_train)
+# pre100 = clf.predict(features_test)
+# print(accuracy_score(labels_test, pre100))
+
+# clf=svm.SVC(kernel='rbf', C=1000)
+# clf.fit(features_train,labels_train)
+# pre1000 = clf.predict(features_test)
+# print(accuracy_score(labels_test, pre1000))
+
+# clf=svm.SVC(kernel='rbf', C=1000
+# clf.fit(features_train,labels_train)
+# pre10000 = clf.predict(features_test)
+# print(accuracy_score(labels_test, pre10000))
+
+clf=svm.SVC(kernel='rbf', C=10000)
+clf.fit(features_train,labels_train)
+pre10000 = clf.predict(features_test)
+
+# print(accuracy_score(labels_test, pre10000))
 #########################################################
 
+# print(pre10000[10])
+# print(pre10000[26])
+# print(pre10000[50])
 
+print(Counter(pre10000))
